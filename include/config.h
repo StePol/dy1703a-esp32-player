0ea4252ab96253ca780416aa74b692502d5a5bea
Štefan Poláčik <stp.supran@gmail.com>
2026-09-12 10:52:10 +0200

    Obnova projektového súboru do main

--- include/config.h
+++ include/config.h
@@ -1,15 +1,26 @@
 #pragma once
 
+// --- UART k DY1703A ---
 #define DY_UART_NUM     2
 #define DY_TX_PIN       17
 #define DY_RX_PIN       16
 #define DY_BAUD_RATE    9600
 
+// --- Tlačidlá pre priamu voľbu skladby (aktívne LOW) ---
+// KEY1 -> skladba 1 -> GPIO25
+// KEY2 -> skladba 2 -> GPIO33
+// KEY3 -> skladba 3 -> GPIO32
+// KEY4 -> skladba 4 -> GPIO35
+// KEY5 -> skladba 5 -> GPIO13
+// KEY6 -> skladba 6 -> GPIO14
+// KEY7 -> skladba 7 -> GPIO27
+// KEY8 -> skladba 8 -> GPIO26
 constexpr uint8_t TRACK_BUTTON_PINS[8] = {25, 33, 32, 35, 13, 14, 27, 26};
 constexpr uint8_t INPUT_ONLY_PIN_COUNT = 1;
 constexpr uint8_t INPUT_ONLY_PINS[INPUT_ONLY_PIN_COUNT] = {35};
 #define BTN_DEBOUNCE_MS 40
 
+// --- WiFi / web rozhranie ---
 #define WIFI_AP_SSID    "DY1703A-Player"
 #define WIFI_AP_PASS    "password"
 #define WEB_SERVER_PORT 80
@@ -19,17 +30,23 @@ constexpr uint8_t INPUT_ONLY_PINS[INPUT_ONLY_PIN_COUNT] = {35};
 #define WIFI_CONNECT_TIMEOUT_MS 90000UL
 #define WIFI_DISCONNECT_GRACE_MS 60000UL
 
+// --- BLE ---
 #define BLE_DEVICE_NAME "DY1703A-Player"
 
+// --- Batéria ---
 #define BATTERY_ADC_PIN     34
 #define BATTERY_ENABLE_PIN  23
 #define BATTERY_R_TOP       100000.0f
 #define BATTERY_R_BOTTOM    100000.0f
 #define BATTERY_READ_INTERVAL_MS 10000
 
+// --- Vibračný motorček ---
+// Motorček je pripojený cez pôvodné dvojtranzistorové zapojenie priamo na ESP32.
+// GPIO4 ovláda tranzistorový stupeň. LOG1 motor zapína.
 #define MOTOR_PIN 4
 #define MOTOR_ACTIVE_LEVEL HIGH
 #define MOTOR_POLL_INTERVAL_MS 300
 
+// --- Light sleep pri nečinnosti ---
 #define INACTIVITY_TIMEOUT_MS         30000UL
 #define LIGHT_SLEEP_CHECK_INTERVAL_MS 5000UL
